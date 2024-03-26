e8f7f13bc4c6b03fab8a37bbafb75b9c743eb398
CalebHensley <157909870+CalebHensley@users.noreply.github.com>
2024-03-25 20:34:50 -0400

    Corrected the database name to match my webhosts DB name

--- Phase04/private/db_credentials.php
+++ Phase04/private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
-// local
+
 define("DB_SERVER", "localhost");
 define("DB_USER", "u676936630_sally");
 define("DB_PASS", "SallyPhase4");
-define("DB_NAME", "salamanders");
+define("DB_NAME", "u676936630_salamanders");
